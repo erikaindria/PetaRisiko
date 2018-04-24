679659c0cc7115e9bd67c40163cbe0695aa26585
Erika Indria <erinds.eis@gmail.com>
2018-04-24 17:58:39 +0700

    crud periode

---
+++ backend/models/Periode.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "periode".
+ *
+ * @property integer $id_periode
+ * @property integer $tahun
+ */
+class Periode extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'periode';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id_periode'], 'required'],
+            [['id_periode', 'tahun'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_periode' => 'Id Periode',
+            'tahun' => 'Tahun',
+        ];
+    }
+}
